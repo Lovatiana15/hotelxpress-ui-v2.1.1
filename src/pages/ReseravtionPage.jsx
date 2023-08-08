693412b66b1fa7e936c35604259edc2d88c3c721
Lovatiana15 <hei.lovatiana@gmail.com>
2023-08-08 07:57:57 +0200

    feat: add new file about reservation page

--- src/pages/ReseravtionPage.jsx
+++ src/pages/ReseravtionPage.jsx
@@ -4,7 +4,6 @@ import { Form, Input, Radio, DatePicker} from 'antd';
 
 
 const ReservationPage = () => {
-    
     const [paymentMethod, setPaymentMethod] = useState('');
     const [arrivalDate, setArrivalDate] = useState(null);
     const [departureDate, setDepartureDate] = useState(null);
@@ -36,30 +35,7 @@ const ReservationPage = () => {
             <div className="bg-white p-4 rounded-xl shadow-xl">
                 <div className="flex flex-col md:flex-row md:space-x-4">
                     <div className="flex-1">
-                        <h2 className="text-xl font-semibold mb-2">Ajouter vos coordonnées</h2>
-                        <Form layout="vertical" onFinish={onFinish}>
-                            <Form.Item
-                                label="Nom"
-                                name="lastName"
-                                rules={[{ required: true, message: 'Veuillez entrer votre nom' }]}
-                            >
-                                <Input style={{ color: 'black', border: '1px solid black' }}/>
-                            </Form.Item>
-                            <Form.Item
-                                label="Prénom"
-                                name="firstName"
-                                rules={[{ required: true, message: 'Veuillez entrer votre prénom' }]}
-                            >
-                                <Input style={{ color: 'black', border: '1px solid black' }}/>
-                            </Form.Item>
-                            <Form.Item
-                                label="Adresse"
-                                name="address"
-                                rules={[{ required: true, message: 'Veuillez entrer votre adresse' }]}
-                            >
-                                <Input style={{ color: 'black', border: '1px solid black' }}/>
-                            </Form.Item>
-                        </Form>
+                        <img src="https://st3.depositphotos.com/2019955/15658/i/450/depositphotos_156586568-stock-photo-woman-in-flowered-dress-holding.jpg" alt="#" />
                     </div>
                     <div className="flex-1">
                         <h2 className="text-xl font-semibold mb-2">Dates de séjour</h2>
@@ -78,78 +54,15 @@ const ReservationPage = () => {
                             >
                                 <DatePicker onChange={onDepartureDateChange} style={{ width: '100%' }} />
                             </Form.Item>
-                        </Form>
-                    </div>
-                    <div className="flex-1">
-                        <h2 className="text-xl font-semibold mb-2">Méthode de paiement</h2>
-                        <Form layout="vertical" onFinish={onFinish}>
-                            <Form.Item
-                                name="paymentMethod"
-                                rules={[{ required: true, message: 'Veuillez sélectionner une méthode de paiement' }]}
-                            >
-                                <Radio.Group onChange={handlePaymentMethodChange}>
-                                    <Radio value="carte">Par carte de crédit</Radio>
-                                    <Radio value="paypal">Paypal</Radio>
-                                    <Radio value="cash">En espèces</Radio>
-                                </Radio.Group>
-                            </Form.Item>
-                            {paymentMethod === 'carte' && (
-                                <div>
-                                    <Form.Item
-                                        label="Numéro de carte"
-                                        name="cardNumber"
-                                        rules={[{ required: true, message: 'Veuillez entrer le numéro de carte' }]}
-                                    >
-                                        <Input style={{ color: 'black', border: '1px solid black' }}/>
-                                    </Form.Item>
-                                    <Form.Item
-                                        label="Date d'expiration"
-                                        name="expirationDate"
-                                        rules={[{ required: true, message: 'Veuillez sélectionner la date d\'expiration' }]}
-                                    >
-                                        <DatePicker picker="month" format="MM/YYYY"/>
-                                    </Form.Item>
-                                    <Form.Item
-                                        label="Code de sécurité"
-                                        name="securityCode"
-                                        rules={[{ required: true, message: 'Veuillez entrer le code de sécurité' }]}
-                                    >
-                                        <Input style={{ color: 'black', border: '1px solid black' }}/>
-                                    </Form.Item>
-                                </div>
-                            )}
-                            {paymentMethod === 'paypal' && (
-                                <div>
-                                    <Form.Item
-                                        label="Adresse e-mail Paypal"
-                                        name="paypalEmail"
-                                        rules={[{ required: true, message: 'Veuillez entrer l\'adresse e-mail Paypal' }]}
-                                    >
-                                        <Input style={{ color: 'black', border: '1px solid black' }}/>
-                                    </Form.Item>
-                                    <Form.Item
-                                        label="Password Paypal"
-                                        name="password"
-                                        rules={[
-                                            { required: true, message: 'Veuillez saisir votre mot de passe' },
-                                        ]}
-                                    >
-                                        <Input.Password prefix={<LockOutlined />}  style={{ color: 'black', border: '1px solid black' }} />
-                                    </Form.Item>
-                                </div>
-                            )}
-                            {paymentMethod === 'cash' && (
-                                <div>
-                                    <p>Le paiement en espèces se fera à l arrivée.</p>
-                                </div>
-                            )}
                             <Form.Item>
                                 <button className="bg-orange-500 text-white px-4 py-2 rounded-lg border border-orange-500 hover:bg-transparent hover:border-orange-500 hover:text-black">
-                                    <p className='text-semibold text-xl'>Reserver</p>
+                                    <p className='text-semibold text-xl'>Confirmer</p>
                                 </button>
                             </Form.Item>
                         </Form>
                     </div>
+
+                  
                 </div>
             </div>
         </div>
